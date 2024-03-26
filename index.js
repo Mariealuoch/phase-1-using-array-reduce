356a548622dab52a1810ad6bc1f555c6c7a6a6e3
TITAN <aluochmarion7@gmail.com>
2024-03-26 14:13:11 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const batteries = [
+    { type: 'AA', count: 10 },
+    { type: 'AAA', count: 6 },
+    { type: '9V', count: 15 }
+  ];
+  
+  const totalBatteries = batteries.reduce(function(total, battery) {
+    return total + battery.count;
+  }, 0);
+  
+  console.log(totalBatteries); 
+  
